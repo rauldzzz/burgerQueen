3abf1c16ede3917e82372e6de8ea5dacc06dbc54
Marcel-Medina <marcel.medina01@estudiant.upf.edu>
2026-05-25 11:37:16 +0200

    Canvi moviment player

--- Assets/Scripts/New Scripts/Marcel_Scripts/PlayerController.cs
+++ Assets/Scripts/New Scripts/Marcel_Scripts/PlayerController.cs
@@ -5,12 +5,14 @@ public class PlayerController : MonoBehaviour
     Vector3 previousPosition;
 
     Vector3 movementVector;
+    [SerializeField] private float rotationThreshold = 0.0001f;
+    [SerializeField] private float rotationSpeed = 10f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        previousPosition = Vector3.zero;
+        previousPosition = transform.position;
         movementVector = Vector3.zero;
 
     }
@@ -18,8 +20,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementVector = Vector3.Lerp(movementVector, transform.position - previousPosition, 0.1f);
-        transform.LookAt(transform.position + movementVector);
+        Vector3 delta = transform.position - previousPosition;
+        movementVector = Vector3.Lerp(movementVector, delta, 0.1f);
+
+        if (movementVector.sqrMagnitude > rotationThreshold)
+        {
+            Quaternion target = Quaternion.LookRotation(movementVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, rotationSpeed * Time.deltaTime);
+        }
+
         previousPosition = transform.position;
     }
 }
